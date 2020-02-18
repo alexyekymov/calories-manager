0b8ba4fa9d798c5f19ac5810408190d5bfbf72e0
alexyekymov <alexyekymov@gmail.com>
2020-02-18 22:55:25 +0200

    renamed and added toString

--- src/main/java/com/alex/util/TimeUtil.java
+++
@@ -1,9 +0,0 @@
-package com.alex.util;
-
-import java.time.LocalTime;
-
-public class TimeUtil {
-    public static boolean isBetweenInclusive(LocalTime localTime, LocalTime startTime, LocalTime endTime) {
-        return localTime.compareTo(startTime) >= 0 && localTime.compareTo(endTime) <= 0;
-    }
-}
