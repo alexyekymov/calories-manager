878e901d26658d363a651c8f831ce8427267ffad
alexyekymov <alexyekymov@gmail.com>
2020-02-21 04:40:50 +0200

    CRUD impl

--- src/main/java/com/alex/web/MealServlet.java
+++ src/main/java/com/alex/web/MealServlet.java
@@ -1,23 +1,80 @@
 package com.alex.web;
 
+import com.alex.model.Meal;
+import com.alex.repository.InMemoryMealRepository;
 import com.alex.util.MealsUtil;
 import org.slf4j.Logger;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
+    private InMemoryMealRepository repository;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        repository = new InMemoryMealRepository();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String id = req.getParameter("id");
+
+        Meal meal = new Meal(id.isEmpty() ? null : Integer.valueOf(id),
+                LocalDateTime.parse(req.getParameter("dateTime")),
+                req.getParameter("description"),
+                Integer.parseInt(req.getParameter("calories")));
+        log.info(meal.isNew() ? "Create new meal" : "Update {}", meal.getId());
+        repository.save(meal);
+        resp.sendRedirect("meals");
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        log.info("get all");
-        req.setAttribute("meals", MealsUtil.getTos(MealsUtil.MEALS, MealsUtil.CALORIES_PER_DAY));
-        req.getRequestDispatcher("/meals.jsp").forward(req, resp);
+        String action = req.getParameter("action");
+
+        switch (action == null ? "all" : action) {
+            case "delete":
+                int id = getId(req);
+                log.info("Delete {}", id);
+                repository.delete(id);
+                resp.sendRedirect("meals");
+                break;
+            case "create":
+            case "update":
+                final Meal meal = "create".equals(action) ?
+                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
+                        repository.get(getId(req));
+//                req.setAttribute("meal", meal);
+//                req.getRequestDispatcher("/mealForm.jsp").forward(req, resp);
+                req.setAttribute("meals",
+                        MealsUtil.getTos(repository.getAll(), MealsUtil.CALORIES_PER_DAY));
+                req.getRequestDispatcher("/meals.jsp").forward(req, resp);
+                break;
+            case "all":
+            default:
+                log.info("get all");
+                req.setAttribute("meals",
+                        MealsUtil.getTos(repository.getAll(), MealsUtil.CALORIES_PER_DAY));
+                req.getRequestDispatcher("/meals.jsp").forward(req, resp);
+                break;
+        }
+    }
+
+    private Integer getId(HttpServletRequest req) {
+        String paramId = Objects.requireNonNull(req.getParameter("id"));
+        return Integer.parseInt(paramId);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -40,11 +40,46 @@
     <h3><a href="index.html">Home</a></h3>
     <hr>
     <h2>Meals</h2>
+    <br><br>
+    <form method="post" action="meals">
+        <input type="hidden" value="${param.id}" name="id">
+        <table>
+            <tr>
+                <th>Date</th>
+                <th>Description</th>
+                <th>Calories</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td>
+                    <label>
+                        <input type="datetime-local" value="${param.date ne null ? param.date : ''}" name="dateTime" required>
+                    </label>
+                </td>
+                <td>
+                    <label>
+                        <input type="text" value="${param.description ne null ? param.description : ''}" name="description" required>
+                    </label>
+                </td>
+                <td>
+                    <label>
+                        <input type="number" value="${param.calories ne null ? param.calories : ''}" name="calories" required>
+                    </label>
+                </td>
+                <td>
+                    <button type="submit">Save</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+
+    <br><br>
     <table>
         <tr>
             <th>Date</th>
             <th>Description</th>
             <th>Calories</th>
+            <th colspan="2" style="text-align: center">Actions</th>
         </tr>
         <jsp:useBean id="meals" scope="request" type="java.util.List"/>
         <c:forEach items="${meals}" var="meal">
@@ -55,6 +90,10 @@
                 </td>
                 <td>${meal.description}</td>
                 <td>${meal.calories}</td>
+                <td>
+                    <a href="meals?action=update&id=${meal.id}&date=${meal.dateTime}&description=${meal.description}&calories=${meal.calories}">Update</a>
+                </td>
+                <td><a href="meals?action=delete&id=${meal.id}">Delete</a></td>
             </tr>
         </c:forEach>
     </table>
