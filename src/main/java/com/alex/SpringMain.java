4c407d411cba55b0b48349a3e61d17f2f36db136
alexyekymov <alexyekymov@gmail.com>
2020-02-29 00:42:33 +0200

    test DI

--- src/main/java/com/alex/SpringMain.java
+++ src/main/java/com/alex/SpringMain.java
@@ -1,6 +1,9 @@
 package com.alex;
 
+import com.alex.model.Role;
+import com.alex.model.User;
 import com.alex.repository.UserRepository;
+import com.alex.service.UserService;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,6 +16,10 @@ public class SpringMain {
 
         UserRepository userRepository = appCtx.getBean(UserRepository.class);
         userRepository.getAll();
+
+        UserService userService = appCtx.getBean(UserService.class);
+        userService.create(new User(null, "userName", "email@google.com", "paasword", Role.ROLE_ADMIN));
+
         appCtx.close();
     }
 }
--- src/main/java/com/alex/service/UserService.java
+++ src/main/java/com/alex/service/UserService.java
@@ -11,6 +11,10 @@ import static com.alex.util.ValidationUtil.checkNotFoundWithId;
 public class UserService {
     private UserRepository repository;
 
+    public void setRepository(UserRepository repository) {
+        this.repository = repository;
+    }
+
     public User create(User user) {
         return repository.save(user);
     }
